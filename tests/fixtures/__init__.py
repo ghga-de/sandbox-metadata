9049d9965c81a2f2a706d3bb20aad0ccf110e960
deepakunni3 <deepak.unni3@gmail.com>
2021-08-09 09:15:46 +0000

    Update fixtures

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -4,7 +4,6 @@ import json
 import pytest
 from fastapi.testclient import TestClient
 from metadata_service.api import app
-from metadata_service.config import get_config
 import mongomock
 
 
@@ -22,23 +21,6 @@ def initialize_test_db():
         objects = json.load(open(os.path.join(curr_dir, '..', '..', 'examples', file)))
         client['test-metadata'][collection_name].delete_many({})
         client['test-metadata'][collection_name].insert_many(objects[file.split(".")[0]])
-
-
-@pytest.fixture(scope="function")
-def initialize_test_db():
-    """Initalize a test metadata store"""
-    config = get_config()
-    curr_dir = os.path.dirname(__file__)
-    json_files = [
-        ("datasets.json", "dataset"),
-        ("studies.json", "study"),
-        ("experiments.json", "experiment")
-    ]
-    for file, collection_name in json_files:
-        objects = json.load(open(os.path.join(curr_dir, '..', '..', 'examples', file)))
-        client = pymongo.MongoClient(config.db_url)
-        client[config.db_name][collection_name].delete_many({})
-        client[config.db_name][collection_name].insert_many(objects[file.split(".")[0]])
 
 
 @pytest.fixture(scope="session")
