57f4ecd019803490aeb7119698068a4c194b8440
deepakunni3 <deepak.unni3@gmail.com>
2021-08-10 11:53:53 +0000

    Add typer cli to generate_openapi_yaml.py

--- scripts/generate_openapi_yaml.py
+++ scripts/generate_openapi_yaml.py
@@ -1,14 +1,27 @@
-import sys
+# Copyright 2021 Universität Tübingen, DKFZ and EMBL
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import typer
 import yaml
 import requests
 
 
-PATH = "http://localhost:8000/openapi.json"
+def main(url: str = "http://localhost:8000/openapi.json", output: str = "openapi.yaml"):
+    response = requests.get(url)
+    json_data = response.json()
+    yaml.dump(json_data, open(output, "w"))
 
 
-if len(sys.argv) > 1:
-    PATH=sys.argv[1]
-
-response = requests.get(PATH)
-json_data = response.json()
-print(yaml.dump(json_data))
+if __name__ == "__main__":
+    typer.run(main)
